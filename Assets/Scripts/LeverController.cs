7d6085848a210600edad90c4f0c732301c329ac9
waerr <fnr15@icloud.com>
2023-01-08 04:17:14 +0000

    Commiting all of the project files

---
+++ Assets/Scripts/LeverController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverController : MonoBehaviour
+{
+    public SpriteRenderer leverSprite;
+    public Sprite leverToggled;
+    public Sprite leverUntoggled;
+    public bool leverTurned = false;
+    private bool isInRange;
+    private int num = 1;
+    float timer = 1f;
+    float delay = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            if (Input.GetKey(KeyCode.E) && isInRange & (num % 2) == 1)
+            {
+                leverSprite.sprite = leverToggled;
+                leverTurned = true;
+                num += 1;
+                timer = delay;
+            }
+            else if (Input.GetKey(KeyCode.E) && isInRange && leverTurned && (num % 2) == 0)
+            {
+                leverSprite.sprite = leverUntoggled;
+                leverTurned = false;
+                num -= 1;
+                timer = delay;
+
+            }
+        }
+    }
+
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
+        
+    }
+    
+    
+    
+}
---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+public class TimerController : MonoBehaviour
+{
+    
+    
+    public static TimerController instance;
+    public Text timeCounter;
+    public Text timerEnd;
+    private TimeSpan timePlaying;
+    public bool timerGoing;
+    private float elapsedTime;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        timeCounter.text = "Time: 00:00.00";
+        timerGoing = false;
+    }
+
+    public void BeginTimer()
+    {
+        timerGoing = true;
+        elapsedTime = 0f;
+
+        StartCoroutine(UpdateTimer());
+    }
+
+    public void EndTimer()
+    {
+        timerGoing = false;
+        Time.timeScale = 0;
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while (timerGoing)
+        {
+            elapsedTime += Time.deltaTime;
+            timePlaying = TimeSpan.FromSeconds(elapsedTime);
+            string timePlayingStr = "Time: " + timePlaying.ToString("mm':'ss'.'ff");
+            timeCounter.text = timePlayingStr;
+ 
+            yield return null;
+        }
+        if (!timerGoing)
+        {
+            string timePlayingStr = "Time Taken: " + timePlaying.ToString("mm':'ss'.'ff");
+            timerEnd.text = timePlayingStr;
+            
+        }
+    }
+}
+
